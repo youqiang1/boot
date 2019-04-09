47c7a79e2b9bcb8af0aa73633c24ac6bb4f76c44
youqiang1 <youqiang@unioncast.cn>
2019-04-09 19:53:43 +0800

    rabbitmq + eureka client

---
+++ kernel/src/main/java/com/yq/kernel/constants/GlobalConstants.java
@@ -0,0 +1,19 @@
+package com.yq.kernel.constants;
+
+/**
+ * <p> 全局常量</p>
+ * @author youq  2019/4/9 17:43
+ */
+public class GlobalConstants {
+
+    /**
+     * 成功
+     */
+    public static final String SUCCESS = "success";
+
+    /**
+     * 失败
+     */
+    public static final String FAIL = "fail";
+
+}
